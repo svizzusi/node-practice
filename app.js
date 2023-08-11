92d46c534094366293c8b6a514f59343d77bf4b5
Stephen Vizzusi <svizzusi13@gmail.com>
2023-08-11 13:59:54 -0700

    Added EJS and Backend redering

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const express = require('express')
 const app = express()
 const PORT  = 2121
+app.set('view engine', 'ejs')
+
+// Use this set if you want to use a different folder than "views" folder
+// app.set('views', 'pages')
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
 
 
+const user = [
+    {
+        userName: "Stephen",
+        location: "San Jose"
+    },
+    {
+        userName: "David",
+        location: "Lagos"
+    }
+]
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, world!</h1>')
+    // Rendered from the backend via node and EJS
+    res.render('index', { user: user})
+
+
+    //  Raw Express
+    // res.sendFile('./pages/index.ejs', {root: __dirname})
+
+    // EJS
+    // res.render('index')
+
+    console.log("Sucessfully loaded")
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About, World</h1>')
+    // EJS
+    res.render('about')
+
+    //  Raw Express
+    // res.sendFile('./pages/about.html', {root: __dirname})
 })
 
 app.use((req, res) => {
-    res.send('Error')
+    // EJS
+    res.status(404).render('error')
+
+    // raw Express
+    // res.status(404).sendFile('./pages/error.html', {root: __dirname})
+    console.log('Page Not Found: 404')
 })
